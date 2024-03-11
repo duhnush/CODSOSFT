b7bf63b27a540fd6e7349b2c19f8a0d4209806f6
Dhanush <81028546+duhnush@users.noreply.github.com>
2024-03-11 22:07:25 +0530

    Update main.js

--- task2/main.js
+++ task2/main.js
@@ -1,25 +1,15 @@
 let menuIcon=document.querySelector('#menu-icon');
-let navbar=document.querySelector('.mavbar');
+let navbar=document.querySelector('.navbar');
 
 menuIcon.onclick=()=>{
     menuIcon.classList.toggle('fa-xmark');
     navbar.classList.toggle('active');
 }
 
-let sections=document.querySelectorAll('section');
-let navLinks=document.querySelectorAll('header nav a');
-
-window.onscroll=()=>{
-    sections.forEach(sec=>{
-        let top=window.scrollY;
-        let offset=sec.offsetTop - 150;
-        let height=sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height){
-            navLinks.forEach.apply(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-}
+const typed=new Typed('.multiple',{
+    strings:['Frontend Developer ','Full Stack Developer','Web Designer'],
+    typeSpeed:70,
+    backSpeed:70,
+    backDelay:1000,
+    loop:true,
+})
